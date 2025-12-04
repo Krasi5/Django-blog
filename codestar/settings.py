fb42789748c9faeee4282b25216f0e5effbbff85
Krasi5 <krasi_todorova@yahoo.com>
2025-12-04 08:50:31 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jq^#i2(sjb00!=##u(sj_7=dky*%hu@apv70yzl6ceuk45mi2e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['Herokuapp', '127.0.0.1',]
 
 
 # Application definition
